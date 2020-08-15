6ef8e8d051025d8ef21daccb29ea9698aacc37c4
Christopher <65589469+fullfatsoda@users.noreply.github.com>
2020-08-15 18:27:12 +0000

    Huge rework
    Learned how to code a lot better over the past 6 months

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -1,141 +1,109 @@
-#! python3
-import random
-import sys
-
-# game loop variable
-game_over = False
-
-# the game object - starts a new game
+from random import randint
+from sys import exit
 
 
 class Game():
-    """A new game"""
-
     def __init__(self):
-        pass
+        self.playing = True
+        self.board = [" " for x in range(9)]
+        self.player = None
+        self.computer = None
 
-    # variables and functions to handle any game board
-    board = b = [
-        " ", " ", " ",
-        " ", " ", " ",
-        " ", " ", " "]
+    def reset(self):
+        self.board = [" " for x in range(9)]
+        self.player = None
+        self.computer = None
+        self.loop()
 
-    # create boards that will check for wins
-    # 0 is used in win conditions so -1 is a blank space
-    moves = {
-        "player": [
-            -1, -1, -1,
-            -1, -1, -1,
-            -1, -1, -1],
-        "computer": [
-            -1, -1, -1,
-            -1, -1, -1,
-            -1, -1, -1]
-    }
-
-
-# draw board
-
-
-def draw_board():
-    b = game.b
-    print(f"""
-  {b[0]} | {b[1]} | {b[2]}
- {"-" * 11}
-  {b[3]} | {b[4]} | {b[5]}
- {"-" * 11}
-  {b[6]} | {b[7]} | {b[8]}
-""")
-
-
-# variables and functions to handle any moves on a board
-# whose move is it?
-player_move = True
-computer_move = False
-
-
-def get_move(_from):
-    b = game.b
-    moves = game.moves
-    # always show the board first
-    draw_board()
-
-    while not game_over:
-        # if it's the players turn
-        if _from:  # player (True)
-            player_move = p = int(input())
-            # if space is empty move is valid
-            if b[p] == " ":
-                b[p] = "o"  # will be visible in the game board when drawn
-                # fills the position on players move board
-                moves['player'][p] = p
-                check_board('player')  # did this move win?
+    def prompt_new_game(self):
+        print("Play again? (y/n)")
+        while True:
+            choice = str(input())
+            if choice == 'y':
                 break
+            elif choice == 'n':
+                exit("Thanks for playing!")
             else:
-                # move is not valid
                 continue
-        else:  # False
-            # its the computer's turn
-            computer_move = c = random.randint(0, 8)
-            # if space is empty move is valid
-            if b[c] == " ":
-                b[c] = "x"  # will be visible in the game board when drawn
-                # fills the position on players move board
-                moves['computer'][c] = c
-                check_board('computer')  # did this move win?
-                break
-            else:
-                # move is not valid
-                continue
+        self.reset()
+
+    def choose_char(self):
+        print("Choose a character to play as, can be any letter, number or character you choose: ")
+        choice = str(input())
+        self.player = Player("Player", choice[0])
+        self.computer = Player("Computer", "$")
 
 
-# check the board for an end game condition
+    def draw(self):
+        b = self.board
+        # draw positions relative to numpad [0] is left blank
+        print(f'\n {b[6]} | {b[7]} | {b[8]}\n{"-" * 11}\n {b[3]} | {b[4]} | {b[5]}\n{"-" * 11}\n {b[0]} | {b[1]} | {b[2]}')
+
+    def get_move(self, move_from):
+        # check if a move is possible
+        if " " not in self.board:
+            make_move = False
+            print("There are no spaces left, the game is a draw.")
+            self.playing = False
+            self.draw()
+            self.prompt_new_game()
+        # allow a move if possible
+        make_move = True
+        while make_move:
+            if move_from == self.player:
+                print("Choose an empty space: ")
+                move = int(input())-1
+                if self.board[move] == " ":
+                    self.board[move] = self.player.char
+                    self.player.moves[move] = move
+                    self.player.check_board()
+                    break
+                else:
+                    continue
+            elif move_from == self.computer:
+                move = randint(0, 8)
+                if self.board[move] == " ":
+                    self.board[move] = self.computer.char
+                    self.computer.moves[move] = move
+                    self.computer.check_board()
+                    break
+                else:
+                    continue
+    
+    def loop(self):
+        self.playing = True
+        self.choose_char()
+        while self.playing:
+            self.draw()
+            self.get_move(self.player)
+            self.get_move(self.computer)
 
 
-def check_board(move_from):
-    b = game.b
-    moves = game.moves
-    # winning lines
-    # 012, 345, 678 horizontal
-    # 036, 147, 258 vertical
-    # 048, 246      diagonal
-    wins = (
-        '012', '345', '678',
-        '036', '147', '258',
-        '048', '246')
 
-    n = 0
-    while n < 8:  # number of win possibilities
-        if move_from == 'player':
-            movelist = moves['player']
-        else:
-            movelist = moves['computer']
+class Player:
+    def __init__(self, name, char):
+        self.name = name
+        self.char = char
+        self.moves = [" " for x in range(9)]
 
-        # use n as tuple position to iterate through possible win scenarios
-        # wins[n][0] is 0 from first position '012' at the start of the loop
-        # if x and y and z are in the players moves on the board
-        # then that is a win condition
-        x, y, z = int(wins[n][0]), int(wins[n][1]), int(wins[n][2])
-        if x in movelist and y in movelist and z in movelist:
-            if move_from == 'player':
-                print("You won.")
-            else:
-                print("Computer wins.")
-            draw_board()
-            sys.exit(0)
-        else:
-            n += 1
-
-    # is it a draw?
-    if " " not in b:
-        # if the board contains no empty spaces it will be a draw because the loop would have found a win before this line
-        print("It's a draw")
-        draw_board()
-        sys.exit(0)
-
-
-# main game loop
-game = Game()
-while not game_over:
-    get_move(player_move)
-    get_move(computer_move)
+    def check_board(self):
+        # winning lines
+            # 012, 345, 678 horizontal
+            # 036, 147, 258 vertical
+            # 048, 246      diagonal
+        wins = (
+            '012', '345', '678',
+            '036', '147', '258',
+            '048', '246')
+        b, m = game.board, self.moves
+        for n in range(len(wins)):
+            a, b, c = int(wins[n][0]), int(wins[n][1]), int(wins[n][2])
+            if a in m and b in m and c in m:
+                print(f"{self.name} wins!")
+                game.playing = False
+                game.draw()
+                game.prompt_new_game()
+                
+if __name__ == "__main__":
+    game = Game()
+    game.loop()
